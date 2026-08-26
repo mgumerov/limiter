37a61880a890adde33fb617aec7324a15526ee7b
Maksim M. Gumerov <mgumerov@jaxel.com>
2026-08-26 17:28:25 +0500

    feat: This way is better

--- server.go
+++ server.go
@@ -7,9 +7,9 @@ import (
 	"os/signal"
 	"os"
 	"syscall"
-	"sync"
-
+	
 	"github.com/gofiber/fiber/v3"
+	recoverer "github.com/gofiber/fiber/v3/middleware/recover"
 )
 
 type Request struct {
@@ -25,36 +25,9 @@ type Processor struct {
 	rqChan chan<- Request
 }
 
-func main() {
-	//тут нам по api key по идее нужно найти бакет
-	//а вообще скорее найти канал в который положить операцию,
-	//ведь запросы выполняются последовательно.
-	
-	// Будет вообще-то
-	// проще если на все будет один канал (вопросы деления на ключи
-	// и потенциально шардирования не будем пока рассматривать)
-
-	// Этот один канал либо должен быть с большим буфером
-	// (но даже так функция тут не сможет вернуть управление клиенту
-	// не дождавшись вердикта)
-	// Какие есть паттерны того чтобы вернуть клиенту ответ асинхронно?
-	// Ну а если канал не имеет буфера, что будет, блокировка? Ну условно конечно, на самом-то деления
-	// горутину просто отложат в сторону.
-
-	// Ну и по хорошему этот канал должен жить в отдельном "сервисе", в смысле что туда будут
-	// кидать обе мои реализации "сервера" - fiber и grpc
-
-	// If you need to guarantee that the receiver actually got the data before the sender moves on, use an unbuffered channel.
-	// - у меня и так и не так, я могу просто закончить но тогда потом на then надо умудриться послать ответ
-	// - что на это соображение скажет ИИ?
-
-	// горутины-воркеры никогда не заканчиваются но зато реюзаются
-	// горутины в семафорной модели (горутины под задачу) заканчиваются и это надежнее (чистый рестарт) - зато не реюзаются
-	// Впрочем "не заканчиваются" для простых рутин не очень плохо, защита от паники как исключений
-	// прицепляется легко, а длительных по времени операций (или обращений куда-то) в моем последнем
-	// варианте вроде бы нет, зависать нечему. А если фатальная ошибка то упадет не одна рутина а все приложение.
-
-	var processor *Processor = startProcessor()
+func main() {	
+	processorFailed := make(chan struct{}, 1)
+	var processor *Processor = startProcessor(processorFailed);
 
 	//The remainder of this function could be just fiber.Listen(), but Fiber v2 does not listen context for graceful
 	// shutdown, neither does it return control after inialization. So, decided to move its startup into a goroutine
@@ -73,42 +46,56 @@ func main() {
 	)
 	defer stop() //we won't need this context after this function completes
 
-	//In general case, with many subsystems like Fiber, we don't know how many errors we might face, 
-	// so we only process only (and we only need 1, because these are critical erros and we halt on them), 
-	// and we use sync.Once to make sure no one is trying to actually send more (and panic because no one listens anymore)
-	fatal := make(chan string, 1)
-	var once sync.Once
-
 	//Idiomatic approach says passing contexts along (here, or/and to fiber.Listen) because this clearly involves some 
 	// i/o and long activity. But actually it depends on how the called func will use the context: maybe I am passing 
 	// request-scoped context to some async processing, for example. In this case, passing globally-scoped notify-context 
 	// actually makes sense, but only if I want Fiber to use it for graceful shutdowns, and I stated above that I prefer it not to.
-	fiber := createHTTP(processor)
-	startHTTP(fiber, func(err string) {
-		//since it's closure and not explicit passing of "once", the language guarantees it's passed by reference,
-		// which is critical (we want to use same instance of this "once" everywhere)
-		once.Do(func() { fatal <- err } )
-	})
-
+	fiberFailed := make(chan struct{}, 1)
+	fiber := createHTTP(processor, fiberFailed)
+	startHTTP(fiber, fiberFailed);
+	
 	select {
-	case err := <- fatal:
-		log.Fatal(err);
-	case <- ctx.Done(): 
+	case <- ctx.Done():
+		//do nothing
+	case <- fiberFailed:
+		log.Print("HTTP server terminated unexpectedly") //since we did not tell it to shut down yet
+		fiber = nil
+	case <- processorFailed:
+		log.Print("Processing Worker terminated unexpectedly") //since we did not tell it to shut down yet
+		fiber = nil
+	
+	}
+	
+	if fiber != nil {
 		if err := fiber.Shutdown(); err != nil {
 			log.Print(err)
 		}
-		processor.close()
 	}
+	processor.close()
 }
 
-func startProcessor() *Processor {
-	reqChan := make(chan Request) //unbuffered, because what good it is under heavy load?
+// In worker-pool approach, worker goroutines never complete, but they are reused and that saves some allocations etc.
+// In semaphore-protected approach, answering routines are completed and cleanly recreated, that's cleaner
+//  (no need to guarantee goroutine will not die out of panic) but more expensive.
+// Also, since I don't want more than 1 request processed at each moment, I can use mutex-protected approach. 
+//  It does not use allocations and does not need goroutine creation at all, and appears like most efficient solution.
+// What do I pick? I'll allow selecting between worker-pool and mutex, because I have some reservation about mutex drawbacks 
+//  and need to do measurements.
+func startProcessor(processorFailed chan<- struct{}) *Processor {
+	reqChan := make(chan Request) //unbuffered, because what good such buffering is - under heavy load?
 	
 	//Process all requests sequentially by a single goroutine
 	go func() {
+		defer func() {
+			//We still want to attempt a controlled termination in main routine, not just crash the app right here
+			if r := recover(); r != nil {
+				log.Printf("Processing Worker panicked: %v", r)
+				processorFailed <- struct{}{}
+			}
+		}()
+
 		limit := 2
 
-		//TODO handle panics
 		//Note, range loop, just like 2-argument reading form, checks for closing the channel,
 		// we'll use it to propagate graceful shutdown to our goroutine
 		for request := range reqChan {
@@ -140,9 +127,22 @@ func (p *Processor) close() { //close is the idiomatic name for stopping lifecyc
 	close(p.rqChan)
 }
 
-func createHTTP(processor *Processor) *fiber.App {
+func createHTTP(processor *Processor, fiberFailed chan<- struct{}) *fiber.App {
 	app := fiber.New()
 
+	app.Use(recoverer.New(recoverer.Config {
+		PanicHandler: 
+			func(c fiber.Ctx, r any) error {
+				fiberFailed <- struct{}{}
+				//Since panics are worse situation in Go than say Java exceptions, we don't look at the error and deduce what
+				// happened and what details to give our client; we don't give any
+				return fiber.ErrInternalServerError
+			}}));
+
+	//Actually we should be looking up a bucket which a specific API call uses (say, calls to one service uses one bucket, calls to other service use another);
+	// and futher development of that idea would be sharding instances by API key.
+	//But let's leave all that out of scope for the sake of being concise: multiple buckets is simple to implement and does not require any learning,
+	// and sharding management is somewhat trickier but makes the project bloat without showing any extra Go mastery.
 	app.Get("/", func(c fiber.Ctx) error {
 		if processor.request(1).granted == 1 {
 			return c.SendString("c.SendStatus(fiber.StatusOK)")
@@ -154,12 +154,24 @@ func createHTTP(processor *Processor) *fiber.App {
 	return app
 }
 
-func startHTTP(fiber *fiber.App, onError func(err string)) {
+func startHTTP(fiber *fiber.App, fiberFailed chan<- struct{}) {
 	go func() {
-		//startup errors return non-nil, graceful shutdown returns nil, shutdown errors are only returned via shutdown()
-		if err := fiber.Listen(":3000"); err != nil { 
-			onError("HTTP Server failed")
-			log.Print(err)
+		defer func() { //always report termination
+			//We still want to attempt a controlled termination in main routine, not just crash the app right here
+			if r := recover(); r != nil {
+        	    log.Printf("HTTP server panicked: %v", r)
+				fiberFailed <- struct{}{}
+        	}
+		} ()
+			
+		//startup errors return non-nil, graceful shutdown returns nil, shutdown errors are only returned via shutdown() - not here
+		if err := fiber.Listen(":3000"); err != nil {
+			log.Printf("HTTP server startup failed: %v", err)
+			fiberFailed <- struct{}{}
 		}
 	}()
 }
+
+//TODO: fill bucket; how?
+//TODO: real bucket algo
+//TODO: mutex + fast path
